d3da25fc8852b70c867e989c4276e5d06947a035
jesipr <jesi.nieves@gmail.com>
2014-11-12 13:42:14 -0400

    Organized better

--- src/RunGame.java
+++ src/RunGame.java
@@ -13,30 +13,58 @@ public class RunGame {
 
 	public static void main(String[] args)  {
 		Scanner in = new Scanner(System.in);
-		System.out.println("Enter Country name");
-		String country = in.nextLine();
-		country = country.replace(' ', '_');
-		System.out.println("Country selected "+ country);
-		Scanner word;
-		try {
-			URL my_url = new URL("http://dbpedia.org/page/"+country);
-//			BufferedReader br = new BufferedReader(new InputStreamReader(my_url.openStream()));
-			word = new Scanner(new InputStreamReader(my_url.openStream()));
-			String strTemp = "";
-			while(null != (strTemp = word.next())){
-				if(strTemp.equalsIgnoreCase("capital")){
-					System.out.println(strTemp);
+		System.out.println("Enter your name.");
+		String name = in.nextLine();
+		CountryGame game = new CountryGame(name);
+		game.startGame();
+
+		in.close();
+	}
+
+
+}
+
+class CountryGame{
+
+	String username;
+	String country;
+
+	public CountryGame(String name){
+		System.out.println("Welcom to the Country Game "+name);
+		this.username=name;
+	}
+
+	public void startGame(){
+		Scanner in = new Scanner(System.in);
+
+		while(true){
+			System.out.println("Enter Country name");
+			country = in.nextLine();
+			country = country.replace(' ', '_');
+			System.out.println("Country selected "+ country);
+
+			try {
+				URL my_url = new URL("http://en.wikipedia.org/wiki/"+country);
+				BufferedReader br = new BufferedReader(new InputStreamReader(my_url.openStream()));
+				//			word = new Scanner(new InputStreamReader(my_url.openStream()));
+				String strTemp = "";
+				while(null != (strTemp = br.readLine())){
+					if(strTemp.contains("Capital"))
+						System.out.println(strTemp);
 				}
+			} catch(NoSuchElementException e) {
+
+			} catch (IOException e) {
+				System.out.println("Could not find that file.");
 			}
-		} catch(NoSuchElementException e) {
-			
-		} catch (MalformedURLException e) {
-			e.printStackTrace();
-		} catch (IOException e) {
-			System.out.println("Could not find that file.");;
-		} finally{
-			in.close();
 		}
+
+	}
+	
+	public boolean checkIfExist(String country) throws IOException{
+		URL countryList_url = new URL("http://www.ibiblio.org/ais/url.htm");
+		BufferedReader br = new BufferedReader(new InputStreamReader(countryList_url.openStream()));
+		return true;
 	}
 
 }
