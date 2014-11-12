5893e47562aaf272d83f3d11f639dad6febd1761
jesi <jesi.nieves@gmail.com>
2014-11-12 18:02:57 -0400

    Added calculations for capital and area
    There are some thinks to tweak. I have to eliminate everything after
    the “&” in the area String.

--- src/RunGame.java
+++ src/RunGame.java
@@ -1,10 +1,8 @@
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
-import java.net.MalformedURLException;
 import java.net.URL;
 import java.util.ArrayList;
-import java.util.NoSuchElementException;
 import java.util.Scanner;
 
 
@@ -26,9 +24,13 @@ public class RunGame {
 }
 
 class CountryGame{
-
+	public final static String KM = "x10000 km^2";
+	
 	private String username;
 	private String country;
+	private String capital;
+	private String population;
+	private String area;
 	private ArrayList<String> selectedCountries = new ArrayList<String>();
 
 
@@ -54,30 +56,19 @@ class CountryGame{
 			}
 			else if(!countryExist(country)){
 				System.out.println("Country does not exist.");
+				System.out.println("You lose");
 				break;
 			}
 
-			country = country.replace(' ', '_');
+			selectedCountries.add(country);
 			System.out.println("Country selected "+ country);
 
-			try {
-				URL my_url = new URL("http://en.wikipedia.org/wiki/"+country);
-				BufferedReader br = new BufferedReader(new InputStreamReader(my_url.openStream()));
-				//			word = new Scanner(new InputStreamReader(my_url.openStream()));
-				String strTemp = "";
-				while(null != (strTemp = br.readLine())){
-					if(strTemp.contains("<b>Capital</b>")){
-						System.out.println(strTemp);
-						break;
-					}
-				}
-			} catch(NoSuchElementException e) {
-
-			} catch (IOException e) {
-				System.out.println("Could not find that file.");
-			}
+			capital = countryCapital(country);
+			area = countryArea(country);
+			
+			System.out.println(capital+" "+ area +" " + " Population ");
 		}
-
+		System.out.println("Thanks for playing");
 		in.close();
 
 	}
@@ -101,7 +92,6 @@ class CountryGame{
 			while(null != (temp = in.readLine())){
 				if(temp.contains(country)){
 					System.out.println("Country exist!");
-					selectedCountries.add(country);
 					return true;
 				}
 			}
@@ -116,12 +106,99 @@ class CountryGame{
 	 * @param country - The country selected
 	 * @return The capital of the selected country
 	 */
-	public String countrysCapital(String country){
+	public String countryCapital(String country){
+		boolean control = false;
+		System.out.println("Looking for " +country +" capital");
+		country = country.replace(' ','_');
 		String capital="";
-
-
-
+		
+		try { 
+			URL countryURL = new URL("http://en.wikipedia.org/wiki/"+country);
+			BufferedReader br = new BufferedReader(new InputStreamReader(countryURL.openStream()));
+			String strTemp = "";
+			while(null != (strTemp = br.readLine())){
+				if(control && strTemp.contains("title=")){
+					System.out.println(strTemp);
+					capital = strTemp.replaceAll("\\<[^>]*>","");
+					capital = capital.trim();
+					break;
+				}else if(strTemp.contains("<b>Capital</b>")){
+					control = true;
+				}
+			}
+		} catch (IOException e) {
+			System.out.println("Could not find country wiki page");
+		}
+		
 		return capital;
+
+	}
+
+	/**
+	 * @param country
+	 * @return
+	 */
+	public String countryPopulation(String country){
+		boolean areaControl = false;
+		boolean totalControl = false;
+		System.out.println("Looking for " +country +" population");
+		country = country.replace(' ','_');
+		String population="";
+		
+		try { 
+			URL countryURL = new URL("http://en.wikipedia.org/wiki/"+country);
+			BufferedReader br = new BufferedReader(new InputStreamReader(countryURL.openStream()));
+			String strTemp = "";
+			while(null != (strTemp = br.readLine())){
+				if(areaControl && totalControl && strTemp.contains("km")){
+					System.out.println(strTemp);
+					population = strTemp.replaceAll("\\<[^>]*>","");
+					population = population.trim();
+					break;
+				}else if(strTemp.contains("Total")){
+					totalControl = true;
+				}else if(strTemp.contains("Area")){
+					areaControl = true;
+				}
+			}
+		} catch (IOException e) {
+			System.out.println("Could not find country wiki page");
+		}
+		return population;
+
+	}
+
+	/**
+	 * @param country
+	 * @return
+	 */
+	public String countryArea(String country){
+		boolean areaControl = false;
+		boolean totalControl = false;
+		System.out.println("Looking for " +country +" population");
+		country = country.replace(' ','_');
+		String area="";
+		
+		try { 
+			URL countryURL = new URL("http://en.wikipedia.org/wiki/"+country);
+			BufferedReader br = new BufferedReader(new InputStreamReader(countryURL.openStream()));
+			String strTemp = "";
+			while(null != (strTemp = br.readLine())){
+				if(areaControl && totalControl && strTemp.contains("km")){
+					System.out.println(strTemp);
+					area = strTemp.replaceAll("\\<[^>]*>","");
+					area = area.trim();
+					break;
+				}else if(strTemp.contains("Total")){
+					totalControl = true;
+				}else if(strTemp.contains("Area")){
+					areaControl = true;
+				}
+			}
+		} catch (IOException e) {
+			System.out.println("Could not find country wiki page");
+		}
+		return area;
 
 	}
 
